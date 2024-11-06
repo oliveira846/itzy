f847f2956dc03c7f77f95d306bb150d928633026
Mauricio de Oliveira Tavares <deoliveiradev846@gmail.com>
2024-11-06 09:12:05 -0300

    a

---
+++ script.js
@@ -0,0 +1,176 @@
+function clicou(){
+    
+    let showMenu = document.querySelector('#social-media')
+
+    if (showMenu.style.display == 'none'){
+        showMenu.style.display = 'block';
+    } else {
+        showMenu.style.display = 'none';
+    }
+}
+
+function album1(){
+    
+    let showMenuAlbum1 = document.querySelector('#content1')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album2(){
+    
+    let showMenuAlbum1 = document.querySelector('#content2')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album3(){
+    
+    let showMenuAlbum1 = document.querySelector('#content3')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album4(){
+    
+    let showMenuAlbum1 = document.querySelector('#content4')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album5(){
+    
+    let showMenuAlbum1 = document.querySelector('#content5')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album6(){
+    
+    let showMenuAlbum1 = document.querySelector('#content6')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album7(){
+    
+    let showMenuAlbum1 = document.querySelector('#content7')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album8(){
+    
+    let showMenuAlbum1 = document.querySelector('#content8')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album9(){
+    
+    let showMenuAlbum1 = document.querySelector('#content9')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album10(){
+    
+    let showMenuAlbum1 = document.querySelector('#content10')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album11(){
+    
+    let showMenuAlbum1 = document.querySelector('#content11')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album12(){
+    
+    let showMenuAlbum1 = document.querySelector('#content12')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album13(){
+    
+    let showMenuAlbum1 = document.querySelector('#content13')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album14(){
+    
+    let showMenuAlbum1 = document.querySelector('#content14')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
+function album15(){
+    
+    let showMenuAlbum1 = document.querySelector('#content15')
+    let displayMenu = document.querySelector('menu')
+
+    if (showMenuAlbum1.style.display == 'none'){
+        showMenuAlbum1.style.display = 'grid';
+    } else {
+        showMenuAlbum1.style.display = 'none';
+    }
+}
